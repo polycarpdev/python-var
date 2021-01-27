e674e412737e64a9b9fa56ae3aadd4e044d33eb9
Polycarp Etyang <polycarprider@gmail.com>
2021-01-27 15:43:11 +0300

    commit message

---
+++ assigning.py
@@ -0,0 +1,14 @@
+#assigning a value to m
+a=b=c=10
+
+print(a)
+print(b)
+print(c)
+
+#assigning different values to multiple variables
+
+a,b,c = 1,29.2, 'python program'
+
+print(a)
+print(b)
+print(c)
---
+++ variable.py
@@ -0,0 +1,9 @@
+
+name = 'polycarp'
+age = 45
+salary = 10,000
+print(age)
+print(name)
+print(salary)
+
+print(name + str('of age') + age + str('earns Kshs.) ' + salary + str('per month'))
